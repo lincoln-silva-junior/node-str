182c7d11c97f997b40b6df86784e7a4d3f650aac
lpinto <lpinto@LPINTOPW1001>
2020-01-21 15:05:08 -0300

    node-str - Initial Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;

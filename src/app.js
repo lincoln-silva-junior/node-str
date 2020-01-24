f1c3b5131e077b9c57ade6194ae7b90fc02cfa56
lpinto <lpinto@LPINTOPW1001>
2020-01-24 16:14:44 -0300

    node-str- 2020-01-24

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
+// PACKAGES *******************
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+// ****************************
+
+// APPLICATION INIT
 const app = express();
 const router = express.Router();
 
+// BODY PARSER
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// ROUTES
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+
+// DATABASE
+mongoose.connect('mongodb://jlincoln:ioNDaRa59@ds054619.mlab.com:54619/node-str'
+                , { useNewUrlParser: true,  useUnifiedTopology: true });
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);

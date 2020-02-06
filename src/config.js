03975ef71a37242881a40509950c713991ff32a6
Lincoln Junior <lincolnj@hotmail.com>
2020-02-05 23:20:10 -0300

    node_str - 2020-02-05_02

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = '34a89da4-bd03-4bdd-bc4f-b44265060cf5';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://jlincoln:ioNDaRa59@ds054619.mlab.com:54619/node-str',
-    sendgridkey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -3,6 +3,7 @@
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
+const emailService = require('../services/email-service');
 
 exports.post = async(req, res, next) => {
     //Validação sem mongoose
@@ -24,6 +25,9 @@ exports.post = async(req, res, next) => {
             email: req.body.email,
             password: md5(req.body.password + global.SALT_KEY)
         });
+
+        emailService.send(req.body.email, 'Bem Vindo ao Node Store', global.EMAIL_TMPL.replace('{0}', req.body.name))
+
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso!'
         });
